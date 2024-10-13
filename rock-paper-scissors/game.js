f2d8be320708c83041feadf88f8be12b230e2948
chenbox <chenbox@gmail.com>
2024-10-13 19:41:03 +0800

    add count animation

--- rock-paper-scissors/game.js
+++ rock-paper-scissors/game.js
@@ -4,10 +4,16 @@ const counter = {
     winCount: 0,
     loseCount: 0,
     tieCount: 0
-};
+}
+
+const winConditions = {
+    Rock: 'Scissors',
+    Paper: 'Rock',
+    Scissors: 'Paper'
+}
 
 function playGame(playerMove) {
-    
+
      // 隨機產生電腦的選擇
     const computerMove = moves[Math.floor(Math.random() * 3)];
 
@@ -16,35 +22,39 @@ function playGame(playerMove) {
     // 判斷結果
     if (playerMove === computerMove) {
         result = 'Tie';
-        counter.tieCount++;
-    } else if (
-        (playerMove === 'Rock' && computerMove === 'Scissors') ||
-        (playerMove === 'Paper' && computerMove === 'Rock') ||
-        (playerMove === 'Scissors' && computerMove === 'Paper')
-    ) {
-        result = 'You Win';
-        counter.winCount++;
+        updateCounterAndAnimate('Tie count', ++counter.tieCount);
+    } else if (winConditions[playerMove] === computerMove) {
+        result = 'You Win'
+        updateCounterAndAnimate('Win count', ++counter.winCount);
     } else {
         result = 'You Lose';
-        counter.loseCount++;
+        updateCounterAndAnimate('Lose count', ++counter.loseCount);
     }
 
     updateUI(playerMove, computerMove, result);
 }   
-    // 顯示結果
+
 function updateUI(playerMove, computerMove, result) {
 
+    //隱藏初始
     document.getElementById("init").classList.add("hidden");
     document.getElementById("result").classList.remove("hidden")
 
+    //勝負說明
     document.getElementById("you-pick").textContent = `${playerMove}.`;
     document.getElementById("computer-pick").textContent = `${computerMove}.`;
     document.getElementById("ending").textContent = `${result}.`;
+}
 
-    document.getElementById("win-count").textContent =
-    (`Win count: ${counter.winCount}`);
-    document.getElementById("lose-count").textContent =
-    (`Lose count: ${counter.loseCount}`);
-    document.getElementById("tie-count").textContent =
-    (`Tie count: ${counter.tieCount}`);
+function updateCounterAndAnimate(elementId, newValue) {
+    const element = document.getElementById(elementId);
+    element.textContent = `${elementId}: ${newValue}`;
+    addBounceAnimation(element);
+}
+
+function addBounceAnimation(element) {
+    element.classList.remove('bounce');
+    setTimeout(() => {
+        element.classList.add('bounce');
+    }, 10);
 }
